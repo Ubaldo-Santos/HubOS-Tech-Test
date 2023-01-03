0b932054d339c2808786338704e3b269a8dd9f26
root <root@WIN-UBALDO-DESKT>
2023-01-03 14:24:51 +0100

    Migrations and seeders for hotels,rooms,users

--- src/routes/web.php
+++ src/routes/web.php
@@ -25,3 +25,21 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// hotels
+Route::get('/hotels', [App\Http\Controllers\HotelController::class, 'index'])->name('hotels');
+Route::get('/hotels/create', [App\Http\Controllers\HotelController::class, 'create'])->name('hotels.create');
+Route::post('/hotels', [App\Http\Controllers\HotelController::class, 'store'])->name('hotels.store');
+Route::get('/hotels/{hotel}', [App\Http\Controllers\HotelController::class, 'show'])->name('hotels.show');
+Route::get('/hotels/{hotel}/edit', [App\Http\Controllers\HotelController::class, 'edit'])->name('hotels.edit');
+Route::put('/hotels/{hotel}', [App\Http\Controllers\HotelController::class, 'update'])->name('hotels.update');
+Route::delete('/hotels/{hotel}', [App\Http\Controllers\HotelController::class, 'destroy'])->name('hotels.destroy');
+
+// rooms
+Route::get('/rooms', [App\Http\Controllers\RoomController::class, 'index'])->name('rooms');
+Route::get('/hotels/{hotel}/rooms/create', [App\Http\Controllers\RoomController::class, 'create'])->name('rooms.create');
+Route::post('/hotels/{hotel}/rooms', [App\Http\Controllers\RoomController::class, 'store'])->name('rooms.store');
+Route::get('/hotels/{hotel}/rooms', [App\Http\Controllers\RoomController::class, 'show'])->name('rooms.show');
+Route::get('/hotels/{hotel}/rooms/{room}/edit', [App\Http\Controllers\RoomController::class, 'edit'])->name('rooms.edit');
+Route::put('/hotels/{hotel}/rooms/{room}', [App\Http\Controllers\RoomController::class, 'update'])->name('rooms.update');
+Route::delete('/hotels/{hotel}/rooms/{room}', [App\Http\Controllers\RoomController::class, 'destroy'])->name('rooms.destroy');
